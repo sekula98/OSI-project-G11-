2ac5b94576f61126ef85318cacb69746019ff7ec
Nidze98 <nikolabursac22@gmail.com>
2019-12-15 15:55:23 +0100

    Brisanje kategorija v(0.3)

--- main.c
+++ main.c
@@ -79,8 +79,10 @@ void kreiranje_def_kat(FILE*f1,FILE*f2,FILE*f3,FILE*f4)
 
 int main()
 {
-    char kor_ime[50],kor_sifra[50],user[50],pass[50],c,nova_kategorija[50],ispis_kat[50];
-    int i=0,x,y;
+    char kor_ime[50],kor_sifra[50],user[50],pass[50],c,nova_kategorija[50],ispis_kat[50],niz_kat[50],**pom,brisi_fajl[50];
+      pom= (char **)malloc(100 * sizeof(char *));
+
+    int i=0,x,y,j,o;
     int dozvola=0;
     FILE*f3,*f4,*f5,*imena_kat;
     citanje_podataka(user,pass);
@@ -108,7 +110,7 @@ int main()
     while(!dozvola);
     do
     {
-        printf("Izaberite: 1.Dodaj novu kategoriju. 2.Prikazi postojece kategorije");
+        printf("\nIzaberite: 1.Dodaj novu kategoriju. 2.Prikazi postojece kategorije. 3.Obrisi kategoriju. 4.Kraj\n");
         scanf("%d",&x);
         if(x==1)
         {
@@ -136,9 +138,56 @@ imena_kat=fopen("imena_kat.txt","r");
 
     }
     }
+    fclose(imena_kat);
     }
+if(x==3)
+    {
+            i=0;
+            imena_kat=fopen("imena_kat.txt","r");
+            if(imena_kat)
+            {
+                printf("Izaberite koju kategoriju zelite da izbrisete\n");
+                while(!feof(imena_kat)) //dodaje kategoriju u niz
+                {
+
+                    fscanf(imena_kat,"%s",niz_kat);
+                    pom[i] = (char *)calloc(strlen(niz_kat) + 1, sizeof(char));
+                    strcpy(pom[i++], niz_kat);
+
+                }
+                for(j=0; j<i; j++)
+                {
+                    printf(" %d. %s",j+1,pom[j]);
+
+                }
+                scanf("%d",&j);
+                j=j-1;
+            }
+            fclose(imena_kat);
+            strcpy(brisi_fajl,pom[j]);//brise datoteku izabrane kategorije
+            strcat(brisi_fajl,".txt");
+            remove(brisi_fajl);//
+            imena_kat=fopen("imena_kat.txt","w");
+            for (o = j ; o <i-1; o++) //brise izabranu kategoriju
+            {
+                pom[o]=pom[o+1];
+            }
+            for(o=0; o<i-1; o++)
+            {
+                fprintf(imena_kat," %s",pom[o]);
+                free(pom[o]);
+            }
+            fclose(imena_kat);
+
+
+        }
+        if (x==4)
+            printf("Kraj");
+
     }
-    while(x==1);
+
+
+    while(x==1||x==2||x==3);
 
 
 
