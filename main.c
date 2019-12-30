63c5ab1334dd4491552d4635d719970620b5f442
Nidze98 <nikolabursac22@gmail.com>
2019-12-30 19:48:39 +0100

    kategorije(fix) v(0.4)

--- main.c
+++ main.c
@@ -18,7 +18,6 @@ void citanje_podataka(char *id,char* pass)
             }
             else
                 strcpy(pass,pom);
-
         }
     }
     else
@@ -52,15 +51,15 @@ char * unos_kor_sifre(char * kor_sifra) //pisanje * umjesto sifre
     return kor_sifra;
 
 }
-void kreiranje_def_kat(FILE*f1,FILE*f2,FILE*f3,FILE*f4)
+void kreiranje_def_kat(FILE*f4)
 {
-    f1=fopen("izlozbe.txt","a");
-    fclose(f1);
-    f2=fopen("koncerti.txt","a");
-    fclose(f2);
-    f3=fopen("promocije.txt","a");
-    fclose(f3);
-    f4=fopen("imena_kat.txt","a");
+//    f1=fopen("izlozbe.txt","a");
+//    fclose(f1);
+//    f2=fopen("koncerti.txt","a");
+//    fclose(f2);
+//    f3=fopen("promocije.txt","a");
+//    fclose(f3);
+//    f4=fopen("imena_kat.txt","a");
     if (NULL != f4)                 //provjera da li je prazna datoteka
     {
         fseek (f4, 0, SEEK_END);
@@ -68,25 +67,81 @@ void kreiranje_def_kat(FILE*f1,FILE*f2,FILE*f3,FILE*f4)
 
         if (0 == size)
         {
-            fprintf(f4,"izlozbe");
-            fprintf(f4," koncerti");
-            fprintf(f4," promocije");
+            fprintf(f4,"izlozbe\n");
+            fprintf(f4,"koncerti\n");
+            fprintf(f4,"promocije\n");
         }
     }
 
     fclose(f4);
 }
+void dodavanje_kategorije(FILE*imena_kat)
+{
+    char nova_kategorija[50];
+    printf("Unesite ime nove kategorije\n");
+    getchar();
+    fgets(nova_kategorija,51,stdin);
+    imena_kat=fopen("imena_kat.txt","a");//upisuje imena kategorija u datoteku
+    fprintf(imena_kat,"%s",nova_kategorija);//
+    fclose(imena_kat);
+}
+void prikaz_kategorija(FILE*imena_kat)
+{
+    char ispis_kat[50];
+    imena_kat=fopen("imena_kat.txt","r");
+    if(imena_kat)
+    {
+        while(fgets(ispis_kat,51,imena_kat))
+        {
+            printf("%s",ispis_kat);
+        }
+    }
+    fclose(imena_kat);
+}
+void brisanje_kategorije(FILE*imena_kat)
+{
+    int i,j,o;
+    char niz_kat[50],**pom,brisi_fajl[50];
+    pom= (char **)malloc(100 * sizeof(char *));
+    i=0;
+    imena_kat=fopen("imena_kat.txt","r");
+    if(imena_kat)
+    {
+        printf("Izaberite koju kategoriju zelite da izbrisete\n");
+        while(fgets(niz_kat,51,imena_kat)) //dodaje kategoriju u niz
+        {
+            pom[i] = (char *)calloc(strlen(niz_kat) + 1, sizeof(char));
+            strcpy(pom[i++], niz_kat);
+        }
+        for(j=0; j<i; j++)
+        {
+            printf(" %d. %s",j+1,pom[j]);
+        }
+        scanf("%d",&j);
+        j=j-1;
+    }
+    fclose(imena_kat);
+    imena_kat=fopen("imena_kat.txt","w");
+    for (o = j ; o <i-1; o++) //brise izabranu kategoriju
+    {
+        pom[o]=pom[o+1];
+    }
+    for(o=0; o<i-1; o++)
+    {
+        fprintf(imena_kat,"%s",pom[o]);
+        free(pom[o]);
+    }
+    fclose(imena_kat);
 
+}
 int main()
 {
-    char kor_ime[50],kor_sifra[50],user[50],pass[50],c,nova_kategorija[50],ispis_kat[50],niz_kat[50],**pom,brisi_fajl[50];
-      pom= (char **)malloc(100 * sizeof(char *));
-
-    int i=0,x,y,j,o;
+    char kor_ime[50],kor_sifra[50],user[50],pass[50];
+    int i=0,x;
     int dozvola=0;
-    FILE*f3,*f4,*f5,*imena_kat;
+    FILE*imena_kat;
     citanje_podataka(user,pass);
-    kreiranje_def_kat(f3,f4,f5,imena_kat);
+    kreiranje_def_kat(imena_kat);
     do
     {
         printf("Unesi ime\n");
@@ -114,78 +169,23 @@ int main()
         scanf("%d",&x);
         if(x==1)
         {
-            printf("Unesite ime nove kategorije\n");
-            scanf("%s",nova_kategorija);
-           imena_kat=fopen("imena_kat.txt","a");//upisuje imena kategorija u datoteku
-            fprintf(imena_kat," %s",nova_kategorija);//
-            fclose(imena_kat);
-
-            strcat(nova_kategorija,".txt");//pravi novu datoteku sa imenom kategorije
-            FILE*f2=fopen(nova_kategorija,"w");
-            fclose(f2);
-
+            dodavanje_kategorije(imena_kat);
         }
 
-if (x==2)
-    {
-imena_kat=fopen("imena_kat.txt","r");
-    if(imena_kat)
-    {
-        while(!feof(imena_kat))
+        if (x==2)
         {
-            fscanf(imena_kat,"%s",ispis_kat);
-            printf(" %s",ispis_kat);
-
-    }
-    }
-    fclose(imena_kat);
-    }
-if(x==3)
-    {
-            i=0;
-            imena_kat=fopen("imena_kat.txt","r");
-            if(imena_kat)
-            {
-                printf("Izaberite koju kategoriju zelite da izbrisete\n");
-                while(!feof(imena_kat)) //dodaje kategoriju u niz
-                {
-
-                    fscanf(imena_kat,"%s",niz_kat);
-                    pom[i] = (char *)calloc(strlen(niz_kat) + 1, sizeof(char));
-                    strcpy(pom[i++], niz_kat);
-
-                }
-                for(j=0; j<i; j++)
-                {
-                    printf(" %d. %s",j+1,pom[j]);
-
-                }
-                scanf("%d",&j);
-                j=j-1;
-            }
-            fclose(imena_kat);
-            strcpy(brisi_fajl,pom[j]);//brise datoteku izabrane kategorije
-            strcat(brisi_fajl,".txt");
-            remove(brisi_fajl);//
-            imena_kat=fopen("imena_kat.txt","w");
-            for (o = j ; o <i-1; o++) //brise izabranu kategoriju
-            {
-                pom[o]=pom[o+1];
-            }
-            for(o=0; o<i-1; o++)
-            {
-                fprintf(imena_kat," %s",pom[o]);
-                free(pom[o]);
-            }
-            fclose(imena_kat);
-
-
+            prikaz_kategorija(imena_kat);
         }
+        if(x==3)
+        {
+            brisanje_kategorije(imena_kat);
+        }
+
         if (x==4)
+        {
             printf("Kraj");
-
+        }
     }
-
 
     while(x==1||x==2||x==3);
 
